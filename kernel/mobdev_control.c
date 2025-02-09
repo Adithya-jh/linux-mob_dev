54e1a0b366a7254fe2cc21e91fb9a9238a9a3c87
adithya-jh <jhadith88@gmail.com>
2025-02-09 22:23:59 +0530

    done with file transfer

--- kernel/mobdev_control.c
+++ kernel/mobdev_control.c
@@ -11,6 +11,7 @@
 #include <linux/rtnetlink.h> // for rtnl_lock/rtnl_unlock
 #include <linux/netlink.h>
 #include <linux/skbuff.h>
+#include <linux/kmod.h>  // For call_usermodehelper()
 
 //
 // Example command IDs
@@ -24,14 +25,13 @@ enum mobdev_cmd {
 
 // This struct can be passed in from user space for commands requiring extra args
 struct mobdev_args {
-    int  enable;
-    char path[128];
-    char ifname[32];  // <-- Add a field for interface name
+    int  enable;       // 1 = push, 0 = pull
+    char path[128];    // File path for transfer
+    char ifname[32];   // Interface name for tethering
 };
 
 //
-// 1) DETECT
-//    Example: Scan USB devices to see if something that looks like a phone is connected.
+// 1) DETECT: Scan USB devices to check for phones
 //
 static int mobdev_detect_cb(struct usb_device *udev, void *data)
 {
@@ -39,194 +39,88 @@ static int mobdev_detect_cb(struct usb_device *udev, void *data)
     unsigned short pid = le16_to_cpu(udev->descriptor.idProduct);
     int cfg_index, if_index;
 
-    // Print out each device for debugging
-    pr_info("mobdev_control: Checking device %04x:%04x (bNumConfigurations=%u)\n",
-            vid, pid, udev->descriptor.bNumConfigurations);
-
-    // We'll scan each configuration and interface to see if anything "looks like a phone."
-    // Many phones present MTP/PTP (class=0x06), RNDIS tethering (class=0xE0), or vendor-specific (class=0xFF).
-    // This is a heuristic, not a guarantee.
+    pr_info("mobdev_control: Checking device %04x:%04x\n", vid, pid);
 
     for (cfg_index = 0; cfg_index < udev->descriptor.bNumConfigurations; cfg_index++) {
         struct usb_host_config *cfg = &udev->config[cfg_index];
-        int num_interfaces = cfg->desc.bNumInterfaces;
 
-        for (if_index = 0; if_index < num_interfaces; if_index++) {
+        for (if_index = 0; if_index < cfg->desc.bNumInterfaces; if_index++) {
             struct usb_interface *interface = cfg->interface[if_index];
-            int alt_index;
 
-            // Each interface can have multiple altsettings.
-            for (alt_index = 0; alt_index < interface->num_altsetting; alt_index++) {
-                struct usb_interface_descriptor *intf_desc =
-                    &interface->altsetting[alt_index].desc;
+            for (int alt_index = 0; alt_index < interface->num_altsetting; alt_index++) {
+                struct usb_interface_descriptor *intf_desc = &interface->altsetting[alt_index].desc;
 
-                pr_info("mobdev_control: Interface class=0x%02x, subclass=0x%02x, protocol=0x%02x\n",
-                        intf_desc->bInterfaceClass,
-                        intf_desc->bInterfaceSubClass,
-                        intf_desc->bInterfaceProtocol);
-
-                // Heuristic: check for MTP/PTP, RNDIS, or vendor-specific classes
                 switch (intf_desc->bInterfaceClass) {
-                case USB_CLASS_STILL_IMAGE: // 0x06 (PTP, often used by MTP)
-                    // MTP is actually a subclass of PTP, so many Android phones show up here
-                    pr_info("mobdev_control: Detected MTP/PTP interface => phone-like\n");
-                    return 1; // Found a phone-like device
-
-                case USB_CLASS_WIRELESS_CONTROLLER: // 0xE0 (RNDIS tethering, etc.)
-                    // Some phones present RNDIS for USB tethering
-                    pr_info("mobdev_control: Detected RNDIS (wireless controller) => phone-like\n");
+                case USB_CLASS_STILL_IMAGE: // 0x06 (MTP/PTP)
+                    pr_info("mobdev_control: Detected MTP/PTP device\n");
                     return 1;
 
-                case USB_CLASS_VENDOR_SPEC: // 0xFF (vendor-specific, common on many Android devices)
-                    pr_info("mobdev_control: Detected vendor-specific interface => phone-like\n");
+                case USB_CLASS_WIRELESS_CONTROLLER: // 0xE0 (RNDIS tethering)
+                    pr_info("mobdev_control: Detected RNDIS device\n");
                     return 1;
 
-                // You could add more cases if you want to catch other classes
+                case USB_CLASS_VENDOR_SPEC: // 0xFF (vendor-specific, common on Android)
+                    pr_info("mobdev_control: Detected vendor-specific device\n");
+                    return 1;
+
                 default:
-                    // Not recognized as a phone-like interface, keep checking
                     break;
                 }
             }
         }
     }
-
-    // If we get here, no interface matched phone-like criteria; keep scanning next device
     return 0;
 }
 
 static long mobdev_detect_phone(void)
 {
-    // usb_for_each_dev stops when the callback returns non-zero.
-    // We'll return 1 if we find a "phone," or 0 if none found.
-    int found = usb_for_each_dev(NULL, mobdev_detect_cb);
-    return found; // 1 => found phone, 0 => no phone
+    return usb_for_each_dev(NULL, mobdev_detect_cb);
 }
 
 // -------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------
-
-
-
 
 //
-// 2) FILE_TRANSFER
-//    For real usage, you'd rely on MTP or mass storage and user-space mounts.
-//    Below is just a placeholder.
+// 2) FILE_TRANSFER: Uses ADB to transfer files
 //
-
-
-
-
-
-
-static int mobdev_detect_mtp_cb(struct usb_device *udev, void *data)
-{
-    int i, j;
-
-    /*
-     * In this callback, we examine each USB device's configs and interfaces
-     * to see if any match MTP/Still Image class = 0x06 or vendor-specific 0xFF.
-     * If we find it, return 1 => "stop iteration and report found."
-     */
-
-    // Iterate over each config
-    for (i = 0; i < udev->descriptor.bNumConfigurations; i++) {
-        struct usb_host_config *cfg = &udev->config[i];
-        int num_ifs = cfg->desc.bNumInterfaces;
-
-        // For each interface in this config
-        for (j = 0; j < num_ifs; j++) {
-            struct usb_interface *intf = cfg->interface[j];
-            int alt;
-            for (alt = 0; alt < intf->num_altsetting; alt++) {
-                struct usb_interface_descriptor *idesc =
-                    &intf->altsetting[alt].desc;
-
-                /*
-                 * MTP often shows up as USB_CLASS_STILL_IMAGE (0x06),
-                 * though subClass/protocol can vary. We'll just check class.
-                 */
-                if (idesc->bInterfaceClass == USB_CLASS_STILL_IMAGE) {
-                    pr_info("mobdev_detect_mtp_cb: Found standard MTP device %04x:%04x\n",
-                        le16_to_cpu(udev->descriptor.idVendor),
-                        le16_to_cpu(udev->descriptor.idProduct));
-
-                    return 1; // Found standard MTP
-                }
-                /*
-                 * Some devices (like OnePlus) use vendor-specific class 0xFF
-                 * We check subClass = 0xFF, protocol = 0x00 to confirm.
-                 */
-                else if (idesc->bInterfaceClass == 0xFF && idesc->bInterfaceSubClass == 0xFF && idesc->bInterfaceProtocol == 0x00) {
-                    pr_info("mobdev_detect_mtp_cb: Found vendor-specific MTP device %04x:%04x\n",
-                        le16_to_cpu(udev->descriptor.idVendor),
-                        le16_to_cpu(udev->descriptor.idProduct));
-
-                    return 1; // Found vendor-specific MTP
-                }
-            }
-        }
-    }
-
-    return 0; // "Didn't find MTP in this device"
-}
-
-static int mobdev_detect_mtp(void)
-{
-    /*
-     * usb_for_each_dev(owner, callback):
-     *  - Iterates over every USB device.
-     *  - If the callback returns non-zero, iteration stops.
-     *  - The non-zero value is returned from usb_for_each_dev().
-     */
-    int found;
-    found = usb_for_each_dev(NULL, mobdev_detect_mtp_cb);
-
-    /*
-     * If found == 1, that means we found an MTP interface.
-     * If 0, no MTP device was found.
-     */
-    return found;
-}
-
-
-
 static long mobdev_file_transfer(struct mobdev_args *args)
 {
-    int ret = mobdev_detect_mtp();
+    int ret = mobdev_detect_phone();
+    char *envp[] = { "HOME=/", "PATH=/sbin:/usr/sbin:/bin:/usr/bin", NULL };
+    char *argv[5];
+
     if (ret == 1) {
-        pr_info("mobdev_control: MTP device present, user-space should do actual file copy.\n");
-        return 0; // success
+        pr_info("mobdev_control: Phone detected, initiating ADB file transfer.\n");
+
+        argv[0] = "/usr/bin/adb";
+        argv[1] = args->enable ? "push" : "pull"; // 1 = push, 0 = pull
+        argv[2] = args->path;
+        argv[3] = args->enable ? "/sdcard/" : "/home/user/";
+        argv[4] = NULL;
+
+        ret = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
+        if (ret < 0) {
+            pr_err("mobdev_control: ADB transfer failed.\n");
+            return -EIO;
+        }
+
+        pr_info("mobdev_control: ADB transfer successful.\n");
+        return 0;
     } else {
         pr_err("mobdev_control: No MTP device found.\n");
         return -ENODEV;
     }
 }
 
-
-
-
-
-
-
 // -------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------
-// -------------------------------------------------------------------------------------------------
-
-
-
 
 //
-// 3) TETHERING
-//    Naive example: Turn a USB network interface on or off (assume "usb0").
+// 3) TETHERING: Controls USB network interfaces
 //
 static long mobdev_tethering(struct mobdev_args *args)
 {
     struct net_device *ndev;
     char ifname[32];
 
-    // Safety: ensure null-terminated
     ifname[0] = '\0';
     strncpy(ifname, args->ifname, sizeof(ifname) - 1);
 
@@ -239,13 +133,11 @@ static long mobdev_tethering(struct mobdev_args *args)
     }
 
     if (args->enable) {
-        // Bring interface up
         if (!(ndev->flags & IFF_UP)) {
             pr_info("mobdev_control: Bringing '%s' up\n", ifname);
             ndev->flags |= IFF_UP;
         }
     } else {
-        // Bring interface down
         if (ndev->flags & IFF_UP) {
             pr_info("mobdev_control: Bringing '%s' down\n", ifname);
             ndev->flags &= ~IFF_UP;
@@ -257,38 +149,37 @@ static long mobdev_tethering(struct mobdev_args *args)
     return 0;
 }
 
+// -------------------------------------------------------------------------------------------------
 
 //
-// 4) NOTIFICATIONS
-//    For demonstration: enable or disable a "fake" notification mechanism.
+// 4) NOTIFICATIONS: Simulates notifications from phone
 //
 static bool notifications_enabled = false;
 
 static void send_fake_notification_to_userspace(void)
 {
-    // Real code might open a netlink socket or similar.
-    pr_info("mobdev_control: Sending a fake notification to user space\n");
+    pr_info("mobdev_control: Sending fake notification to user space\n");
 }
 
 static long mobdev_notifications(struct mobdev_args *args)
 {
     if (args->enable && !notifications_enabled) {
-        pr_info("mobdev_control: Enabling phone notifications\n");
+        pr_info("mobdev_control: Enabling notifications\n");
         notifications_enabled = true;
         send_fake_notification_to_userspace();
     } else if (!args->enable && notifications_enabled) {
-        pr_info("mobdev_control: Disabling phone notifications\n");
+        pr_info("mobdev_control: Disabling notifications\n");
         notifications_enabled = false;
-        // teardown subscription if applicable
     } else {
-        pr_info("mobdev_control: No change in notification subscription\n");
+        pr_info("mobdev_control: No change in notification state\n");
     }
     return 0;
 }
 
+// -------------------------------------------------------------------------------------------------
+
 //
-// The main syscall. We define it via SYSCALL_DEFINE2(...) so that the kernel
-// auto-generates the correct 'sys_mobdev_control' symbol and prototype.
+// System Call Implementation
 //
 SYSCALL_DEFINE2(mobdev_control,
                 unsigned int, cmd,
@@ -297,7 +188,6 @@ SYSCALL_DEFINE2(mobdev_control,
     struct mobdev_args kargs;
     long ret = 0;
 
-    // If command uses arguments, copy from user
     if ((cmd == MOBDEV_FILE_TRANSFER ||
          cmd == MOBDEV_TETHERING   ||
          cmd == MOBDEV_NOTIFICATIONS) && arg != 0) {
@@ -307,7 +197,6 @@ SYSCALL_DEFINE2(mobdev_control,
         }
     }
 
-    // Dispatch by command
     switch (cmd) {
     case MOBDEV_DETECT:
         pr_info("mobdev_control: DETECT command\n");
@@ -318,7 +207,6 @@ SYSCALL_DEFINE2(mobdev_control,
         pr_info("mobdev_control: FILE_TRANSFER command\n");
         ret = mobdev_file_transfer(&kargs);
         break;
-
 
     case MOBDEV_TETHERING:
         pr_info("mobdev_control: TETHERING command\n");
