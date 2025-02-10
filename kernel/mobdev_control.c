daa02fdb333be7da0f15d442db39705495098435
adithya-jh <jhadith88@gmail.com>
2025-02-10 23:03:04 +0530

    tried updating for call

--- kernel/mobdev_control.c
+++ kernel/mobdev_control.c
@@ -8,61 +8,62 @@
 #include <linux/netdevice.h>
 #include <linux/notifier.h>
 #include <linux/device.h>
-#include <linux/rtnetlink.h> // for rtnl_lock/rtnl_unlock
+#include <linux/rtnetlink.h>
 #include <linux/netlink.h>
 #include <linux/skbuff.h>
 #include <linux/kmod.h>  // For call_usermodehelper()
 
 //
-// Example command IDs
+// Supported Commands
 //
 enum mobdev_cmd {
     MOBDEV_DETECT = 0,
     MOBDEV_FILE_TRANSFER,
     MOBDEV_TETHERING,
     MOBDEV_NOTIFICATIONS,
-};
-
-// This struct can be passed in from user space for commands requiring extra args
-struct mobdev_args {
-    int  enable;       // 1 = push, 0 = pull
-    char path[128];    // File path for transfer
-    char ifname[32];   // Interface name for tethering
+    MOBDEV_CALL_CONTROL,  // Value 4
 };
 
 //
-// 1) DETECT: Scan USB devices to check for phones
+// Struct for User Arguments
+//
+struct mobdev_args {
+    int  enable;       // For file transfer/tethering/notifications (1 = push/on, 0 = pull/off)
+    char path[128];    // File path for transfer
+    char ifname[32];   // Interface name for tethering
+    int action;        // For call control: 1 = answer, 0 = reject
+};
+
+// -------------------------------------------------------------------------------------------------
+//
+// 1️⃣ DETECT: Check for connected phones via USB
 //
 static int mobdev_detect_cb(struct usb_device *udev, void *data)
 {
     unsigned short vid = le16_to_cpu(udev->descriptor.idVendor);
     unsigned short pid = le16_to_cpu(udev->descriptor.idProduct);
-    int cfg_index, if_index;
 
     pr_info("mobdev_control: Checking device %04x:%04x\n", vid, pid);
 
-    for (cfg_index = 0; cfg_index < udev->descriptor.bNumConfigurations; cfg_index++) {
+    for (int cfg_index = 0; cfg_index < udev->descriptor.bNumConfigurations; cfg_index++) {
         struct usb_host_config *cfg = &udev->config[cfg_index];
 
-        for (if_index = 0; if_index < cfg->desc.bNumInterfaces; if_index++) {
+        for (int if_index = 0; if_index < cfg->desc.bNumInterfaces; if_index++) {
             struct usb_interface *interface = cfg->interface[if_index];
 
             for (int alt_index = 0; alt_index < interface->num_altsetting; alt_index++) {
                 struct usb_interface_descriptor *intf_desc = &interface->altsetting[alt_index].desc;
 
                 switch (intf_desc->bInterfaceClass) {
-                case USB_CLASS_STILL_IMAGE: // 0x06 (MTP/PTP)
+                case USB_CLASS_STILL_IMAGE:
                     pr_info("mobdev_control: Detected MTP/PTP device\n");
                     return 1;
-
-                case USB_CLASS_WIRELESS_CONTROLLER: // 0xE0 (RNDIS tethering)
+                case USB_CLASS_WIRELESS_CONTROLLER:
                     pr_info("mobdev_control: Detected RNDIS device\n");
                     return 1;
-
-                case USB_CLASS_VENDOR_SPEC: // 0xFF (vendor-specific, common on Android)
+                case USB_CLASS_VENDOR_SPEC:
                     pr_info("mobdev_control: Detected vendor-specific device\n");
                     return 1;
-
                 default:
                     break;
                 }
@@ -78,9 +79,8 @@ static long mobdev_detect_phone(void)
 }
 
 // -------------------------------------------------------------------------------------------------
-
 //
-// 2) FILE_TRANSFER: Uses ADB to transfer files
+// 2️⃣ FILE_TRANSFER: Uses ADB for file transfers
 //
 static long mobdev_file_transfer(struct mobdev_args *args)
 {
@@ -92,7 +92,7 @@ static long mobdev_file_transfer(struct mobdev_args *args)
         pr_info("mobdev_control: Phone detected, initiating ADB file transfer.\n");
 
         argv[0] = "/usr/bin/adb";
-        argv[1] = args->enable ? "push" : "pull"; // 1 = push, 0 = pull
+        argv[1] = args->enable ? "push" : "pull";
         argv[2] = args->path;
         argv[3] = args->enable ? "/sdcard/" : "/home/user/";
         argv[4] = NULL;
@@ -112,16 +112,50 @@ static long mobdev_file_transfer(struct mobdev_args *args)
 }
 
 // -------------------------------------------------------------------------------------------------
-
 //
-// 3) TETHERING: Controls USB network interfaces
+// 3️⃣ CALL CONTROL: Detect & answer/reject calls via ADB
+//
+static long mobdev_call_control(struct mobdev_args *args)
+{
+    char *envp[] = { "HOME=/", "PATH=/sbin:/usr/sbin:/bin:/usr/bin", NULL };
+    char *adb_check_call[] = { "/usr/bin/adb", "shell", "dumpsys", "telephony.registry", NULL };
+    char *adb_answer_call[] = { "/usr/bin/adb", "shell", "input", "keyevent", "KEYCODE_CALL", NULL };
+    char *adb_reject_call[] = { "/usr/bin/adb", "shell", "input", "keyevent", "KEYCODE_ENDCALL", NULL };
+
+    pr_info("mobdev_control: Checking for incoming calls...\n");
+
+    int ret = call_usermodehelper(adb_check_call[0], adb_check_call, envp, UMH_WAIT_PROC);
+    if (ret < 0) {
+        pr_err("mobdev_control: Failed to check call state.\n");
+        return -EIO;
+    }
+
+    pr_info("mobdev_control: Incoming call detected! Processing action...\n");
+
+    /* Use the action field to determine the ADB command:
+       action == 1 means answer the call,
+       action == 0 means reject the call.
+    */
+    char **adb_action = args->action ? adb_answer_call : adb_reject_call;
+    ret = call_usermodehelper(adb_action[0], adb_action, envp, UMH_WAIT_PROC);
+    if (ret < 0) {
+        pr_err("mobdev_control: Failed to process call action.\n");
+        return -EIO;
+    }
+
+    pr_info("mobdev_control: Call processed successfully.\n");
+    return 0;
+}
+
+// -------------------------------------------------------------------------------------------------
+//
+// 4️⃣ TETHERING: Enable/Disable USB tethering
 //
 static long mobdev_tethering(struct mobdev_args *args)
 {
     struct net_device *ndev;
     char ifname[32];
 
-    ifname[0] = '\0';
     strncpy(ifname, args->ifname, sizeof(ifname) - 1);
 
     rtnl_lock();
@@ -150,36 +184,22 @@ static long mobdev_tethering(struct mobdev_args *args)
 }
 
 // -------------------------------------------------------------------------------------------------
-
 //
-// 4) NOTIFICATIONS: Simulates notifications from phone
+// 5️⃣ NOTIFICATIONS: Enable/Disable notification mirroring
 //
-static bool notifications_enabled = false;
-
-static void send_fake_notification_to_userspace(void)
-{
-    pr_info("mobdev_control: Sending fake notification to user space\n");
-}
-
 static long mobdev_notifications(struct mobdev_args *args)
 {
-    if (args->enable && !notifications_enabled) {
-        pr_info("mobdev_control: Enabling notifications\n");
-        notifications_enabled = true;
-        send_fake_notification_to_userspace();
-    } else if (!args->enable && notifications_enabled) {
-        pr_info("mobdev_control: Disabling notifications\n");
-        notifications_enabled = false;
+    if (args->enable) {
+        pr_info("mobdev_control: Notifications enabled\n");
     } else {
-        pr_info("mobdev_control: No change in notification state\n");
+        pr_info("mobdev_control: Notifications disabled\n");
     }
     return 0;
 }
 
 // -------------------------------------------------------------------------------------------------
-
 //
-// System Call Implementation
+// 6️⃣ SYSTEM CALL HANDLER
 //
 SYSCALL_DEFINE2(mobdev_control,
                 unsigned int, cmd,
@@ -188,9 +208,11 @@ SYSCALL_DEFINE2(mobdev_control,
     struct mobdev_args kargs;
     long ret = 0;
 
+    /* For commands that require user arguments, copy the data from user space */
     if ((cmd == MOBDEV_FILE_TRANSFER ||
          cmd == MOBDEV_TETHERING   ||
-         cmd == MOBDEV_NOTIFICATIONS) && arg != 0) {
+         cmd == MOBDEV_NOTIFICATIONS ||
+         cmd == MOBDEV_CALL_CONTROL) && arg != 0) {
         if (copy_from_user(&kargs, (struct mobdev_args __user *)arg, sizeof(kargs))) {
             pr_err("mobdev_control: Failed to copy args from user\n");
             return -EFAULT;
@@ -199,25 +221,21 @@ SYSCALL_DEFINE2(mobdev_control,
 
     switch (cmd) {
     case MOBDEV_DETECT:
-        pr_info("mobdev_control: DETECT command\n");
         ret = mobdev_detect_phone();
         break;
-
     case MOBDEV_FILE_TRANSFER:
-        pr_info("mobdev_control: FILE_TRANSFER command\n");
         ret = mobdev_file_transfer(&kargs);
         break;
-
     case MOBDEV_TETHERING:
-        pr_info("mobdev_control: TETHERING command\n");
         ret = mobdev_tethering(&kargs);
         break;
-
     case MOBDEV_NOTIFICATIONS:
-        pr_info("mobdev_control: NOTIFICATIONS command\n");
         ret = mobdev_notifications(&kargs);
         break;
-
+    case MOBDEV_CALL_CONTROL:
+        pr_info("mobdev_control: CALL_CONTROL command\n");
+        ret = mobdev_call_control(&kargs);
+        break;
     default:
         pr_err("mobdev_control: Unknown command %u\n", cmd);
         ret = -EINVAL;
